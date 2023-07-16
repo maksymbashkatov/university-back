f5d129ff8b66870ad71c48ca06be576f13a0fa53
Maksym Bashkatov <maksymbashkatov.spro@gmail.com>
2023-07-16 19:34:45 +0300

    Was added swagger for marks.

--- src/marks/marks.router.ts
+++ src/marks/marks.router.ts
@@ -7,16 +7,105 @@ import { markCreateSchema } from './mark.schema';
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Marks
+ *   description: The marks managing API
+ * /marks/get-all-by-student/{id}:
+ *   get:
+ *     summary: Get all marks by student id
+ *     tags: [Marks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: Student id
+ *     responses:
+ *       200:
+ *         description: The list of marks by student id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/GetMarkByStudentId'
+ *       404:
+ *         description: Student not found
+ *       500:
+ *         description: Some server error
+ */
 router.get(
   '/get-all-by-student/:id',
   validator.params(idParamSchema),
   controllerWrapper(marksController.getAllMarksByStudentId),
 );
+
+/**
+ * @swagger
+ * tags:
+ *   name: Marks
+ *   description: The marks managing API
+ * /marks/get-all-by-course/{id}:
+ *   get:
+ *     summary: Get all marks
+ *     tags: [Marks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: Course id
+ *     responses:
+ *       200:
+ *         description: The list of marks by course id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/GetMarkByCourseId'
+ *       404:
+ *         description: Course not found
+ *       500:
+ *         description: Some server error
+ */
 router.get(
   '/get-all-by-course/:id',
   validator.params(idParamSchema),
   controllerWrapper(marksController.getAllMarksByCourseId),
 );
+
+/**
+ * @swagger
+ * tags:
+ *   name: Marks
+ *   description: The marks managing API
+ * /marks:
+ *   post:
+ *     summary: Create mark
+ *     tags: [Marks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateMark'
+ *     responses:
+ *       201:
+ *         description: Was created a mark
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/GetMark'
+ *       400:
+ *         description: Validation error
+ *       500:
+ *         description: Some server error
+ */
 router.post(
   '/',
   validator.body(markCreateSchema),
--- src/marks/types/mark-create-request.interface.ts
+++ src/marks/types/mark-create-request.interface.ts
@@ -1,6 +1,36 @@
 import { ContainerTypes, ValidatedRequestSchema } from 'express-joi-validation';
 import { IMark } from './mark.interface';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CreateMark:
+ *       type: object
+ *       required:
+ *         - mark
+ *         - courseId
+ *         - studentId
+ *         - lectorId
+ *       properties:
+ *         mark:
+ *           type: string
+ *           description: The mark
+ *         courseId:
+ *           type: number
+ *           description: The id of the course
+ *         studentId:
+ *           type: number
+ *           description: The id of the student
+ *         lectorId:
+ *           type: number
+ *           description: The id of the lector
+ *       example:
+ *         mark: A
+ *         courseId: 1
+ *         studentId: 1
+ *         lectorId: 1
+ */
 export interface IMarkCreateRequest extends ValidatedRequestSchema {
   [ContainerTypes.Body]: Omit<IMark, 'id'> & {
     courseId: number;
