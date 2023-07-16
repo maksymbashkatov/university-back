3f3a5bd18f82a9a07605b15ae12357b2eca99597
Maksym Bashkatov <maksymbashkatov.spro@gmail.com>
2023-07-16 13:34:52 +0300

    Was added swagger for groups.

--- src/groups/groups.router.ts
+++ src/groups/groups.router.ts
@@ -7,23 +7,159 @@ import { idParamSchema } from '../application/schemas/id-param.schema';
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Groups
+ *   description: The groups managing API
+ * /groups:
+ *   get:
+ *     summary: Get all groups
+ *     tags: [Groups]
+ *     responses:
+ *       200:
+ *         description: The list of groups.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/GetGroupWithStudents'
+ *       500:
+ *         description: Some server error
+ */
 router.get('/', controllerWrapper(groupsController.getAllGroups));
+
+/**
+ * @swagger
+ * tags:
+ *   name: Groups
+ *   description: The groups managing API
+ * /groups/{id}:
+ *   get:
+ *     summary: Get group by id
+ *     tags: [Groups]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: Group id
+ *     responses:
+ *       200:
+ *         description: The list of groups.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/GetGroupWithStudents'
+ *       404:
+ *         description: Group not found
+ *       500:
+ *         description: Some server error
+ */
 router.get(
   '/:id',
   validator.params(idParamSchema),
   controllerWrapper(groupsController.getGroupById),
 );
+
+/**
+ * @swagger
+ * tags:
+ *   name: Groups
+ *   description: The groups managing API
+ * /groups:
+ *   post:
+ *     summary: Create group
+ *     tags: [Groups]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateGroup'
+ *     responses:
+ *       201:
+ *         description: Was created a group
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/GetGroup'
+ *       400:
+ *         description: Validation error
+ *       500:
+ *         description: Some server error
+ */
 router.post(
   '/',
   validator.body(groupCreateSchema),
   controllerWrapper(groupsController.createGroup),
 );
+
+/**
+ * @swagger
+ * tags:
+ *   name: Groups
+ *   description: The groups managing API
+ * /groups/{id}:
+ *   patch:
+ *     summary: Update group
+ *     tags: [Groups]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: Group id
+ *     requestBody:
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UpdateGroup'
+ *     responses:
+ *       204:
+ *         description: No content
+ *       400:
+ *         description: Validation error
+ *       404:
+ *         description: Group not found
+ *       500:
+ *         description: Some server error
+ */
 router.patch(
   '/:id',
   validator.params(idParamSchema),
   validator.body(groupUpdateSchema),
   controllerWrapper(groupsController.updateGroupById),
 );
+
+/**
+ * @swagger
+ * tags:
+ *   name: Groups
+ *   description: The groups managing API
+ * /groups/{id}:
+ *   delete:
+ *     summary: Delete group
+ *     tags: [Groups]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: Group id
+ *     responses:
+ *       204:
+ *         description: No content
+ *       404:
+ *         description: Group not found
+ *       500:
+ *         description: Some server error
+ */
 router.delete(
   '/:id',
   validator.params(idParamSchema),
--- src/groups/types/group-create-request.interface.ts
+++ src/groups/types/group-create-request.interface.ts
@@ -1,6 +1,21 @@
 import { ContainerTypes, ValidatedRequestSchema } from 'express-joi-validation';
 import { IGroup } from './group.interface';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CreateGroup:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the group
+ *       example:
+ *         name: Group3
+ */
 export interface IGroupCreateRequest extends ValidatedRequestSchema {
   [ContainerTypes.Body]: Omit<IGroup, 'id'>;
 }
