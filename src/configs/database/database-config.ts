033e51e6429e92467609387a2112f6c4a085a6a5
Maksym Bashkatov <maksymbashkatov.spro@gmail.com>
2023-07-21 14:17:25 +0300

    Was finished hw docker.

--- src/configs/database/database-config.ts
+++ src/configs/database/database-config.ts
@@ -4,8 +4,11 @@ export const databaseConfiguration = (
   isMigrationRun = true,
 ): DataSourceOptions => {
   const ROOT_PATH: string = process.cwd();
-  const migrationsPath = `${ROOT_PATH}/src/**/migrations/*.ts`;
-  const entitiesPath = `${ROOT_PATH}/src/**/*.entity.ts`;
+  const n_e = process.env.NODE_ENV;
+  const file_extension =
+    n_e === 'production' ? 'js' : n_e === 'development' ? 'ts' : '';
+  const migrationsPath = `${ROOT_PATH}/src/**/migrations/*.${file_extension}`;
+  const entitiesPath = `${ROOT_PATH}/src/**/*.entity.${file_extension}`;
 
   return {
     type: 'postgres',
@@ -18,7 +21,7 @@ export const databaseConfiguration = (
     migrations: [migrationsPath],
     migrationsTableName: 'migrations',
     migrationsRun: isMigrationRun,
-    synchronize: false,
+    synchronize: true,
     logging: true,
   };
 };
